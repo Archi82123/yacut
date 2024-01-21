708a80f127f1e6f42abd0ca33d0cd40bc206d1ba
Артур Устимов <arthur82123@yandex.ru>
2024-01-22 00:54:56 +0300

    Исправления после ревью #1

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 import os
+import string
 
 
 class Config(object):
@@ -8,3 +9,11 @@ class Config(object):
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
+
+
+# Константы
+
+MAX_ORIGINAL_LINK_LENGTH = 128
+MAX_CUSTOM_ID_LENGTH = 16
+MIN_LENGTH = 1
+ALL_CHARS = string.ascii_letters + string.digits
